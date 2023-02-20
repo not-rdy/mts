e903ea86ccf75e3d2a877215e9138f2f38281f4d
Rustem <mr.kamilyanov@mail.ru>
2023-02-20 22:36:25 +0300

    GCN

--- src/lib/models.py
+++ src/lib/models.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-from torch_geometric.nn.aggr import MulAggregation
+from torch_geometric.nn.aggr import MaxAggregation
 
 params_GCN = {
     'device': 'cuda',
-    'batch_size': 1024,
+    'batch_size': 32,
     'lr': 0.01,
     'weight_decay': 5e-4,
     'n_epochs': 100
@@ -17,11 +17,10 @@ class GCN(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.lin1 = torch.nn.Linear(19, 100)
-        self.conv1 = GCNConv(100, 100)
-        self.conv2 = GCNConv(100, 80)
-        self.conv3 = GCNConv(80, 40)
-        self.lin2 = torch.nn.Linear(40, 6)
-        self.agg = MulAggregation()
+        self.conv1 = GCNConv(100, 90)
+        self.conv2 = GCNConv(90, 70)
+        self.lin2 = torch.nn.Linear(70, 6)
+        self.agg = MaxAggregation()
 
     def forward(self, graph):
         x, edge_index = graph.x, graph.edge_index
@@ -31,10 +30,8 @@ class GCN(torch.nn.Module):
         x = F.relu(x)
         x = self.conv2(x, edge_index)
         x = F.relu(x)
-        x = self.conv3(x, edge_index)
-        x = F.relu(x)
         x = F.dropout(x, training=self.training)
-        x = self.lin2(x)
         x = self.agg(x, graph.batch)
+        x = self.lin2(x)
         out = F.softmax(x, dim=0)
         return out
