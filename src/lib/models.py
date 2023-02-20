b890c142db08e0d7ba30bbb150797ec9e569c00f
Rustem <mr.kamilyanov@mail.ru>
2023-02-20 17:39:06 +0300

    GCN v1.1

--- src/lib/models.py
+++ src/lib/models.py
@@ -6,7 +6,7 @@ from torch_geometric.nn.aggr import MulAggregation
 params_GCN = {
     'device': 'cuda',
     'batch_size': 32,
-    'lr': 0.01,
+    'lr': 0.001,
     'weight_decay': 5e-4,
     'n_epochs': 20
 }
