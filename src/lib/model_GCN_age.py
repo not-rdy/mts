556496dc8f27f9050cad9429a51f13951faba388
Rustem <mr.kamilyanov@mail.ru>
2023-03-03 23:09:56 +0300

    interim

--- src/lib/model_GCN_age.py
+++ src/lib/model_GCN_age.py
@@ -6,7 +6,7 @@ from torch_geometric.nn.aggr import MaxAggregation
 params_GCN = {
     'device': 'cuda',
     'batch_size': 16,
-    'lr': 0.01,
+    'lr': 0.001,
     'weight_decay': 5e-4,
     'n_epochs': 50
 }
@@ -18,8 +18,9 @@ class GCN_age(torch.nn.Module):
         super().__init__()
         self.conv1 = GCNConv(19, 500)
         self.conv2 = GCNConv(500, 400)
+        self.conv3 = GCNConv(400, 300)
         self.agg = MaxAggregation()
-        self.lin1 = torch.nn.Linear(400, 200)
+        self.lin1 = torch.nn.Linear(300, 200)
         self.lin2 = torch.nn.Linear(200, 100)
         self.lin3 = torch.nn.Linear(100, 6)
 
@@ -28,6 +29,9 @@ class GCN_age(torch.nn.Module):
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.3, training=self.training)
+        x = self.conv3(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, p=0.3, training=self.training)
         x = self.agg(x, index=graph.batch)
