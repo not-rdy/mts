fbbbed0043cf88978e4accbaa304b422eeec86d3
Rustem <mr.kamilyanov@mail.ru>
2023-03-07 23:09:50 +0300

    .

--- src/lib/params_age.py
+++ src/lib/params_age.py
@@ -1,9 +1,9 @@
 params = {
     'device': 'cuda',
     'batch_size': 16,
-    'lr': 5e-4,
-    'weight_decay': 5e-4,
-    'n_epochs': 10
+    'lr': 5e-5,
+    'weight_decay': 5e-5,
+    'n_epochs': 30
 }
 params_model = {
     'in_channels': 18,
