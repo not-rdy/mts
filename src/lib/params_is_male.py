a209d670e9d171561c158f74cddc264903642b65
Rustem <mr.kamilyanov@mail.ru>
2023-03-10 10:47:23 +0300

    interim

--- src/lib/params_is_male.py
+++ src/lib/params_is_male.py
@@ -1,24 +1,17 @@
 params = {
     'device': 'cuda',
-    'batch_size': 16,
-    'lr': 0.001,
-    'weight_decay': 5e-4,
-    'n_epochs': 10
+    'batch_size': 8,
+    'lr': 5e-5,
+    'weight_decay': 5e-5,
+    'n_epochs': 20
 }
 params_model = {
     'in_channels': 18,
     'hidden_channels': 100,
-    'num_layers': 3,
-    'out_channels': 100,
+    'num_layers': 6,
+    'out_channels': 1,
     'dropout': 0.3,
     'act': 'relu',
     'aggr': 'max',
-    'jk': 'lstm'
-}
-params_agg_lstm = {
-    'in_channels': 100,
-    'out_channels': 6,
-    'num_layers': 2,
-    'dropout': 0.3,
-    'bidirectional': False
+    'jk': 'cat'
 }
--- src/train_is_male.py
+++ src/train_is_male.py
@@ -4,12 +4,11 @@ import mlflow
 from tqdm import tqdm
 from lib.utils import save_f, load_f
 from base.settings import PATH_DATA_INTERIM
-from lib.params_is_male import params, params_model, params_agg_lstm
+from lib.params_is_male import params, params_model
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn.models import GraphSAGE
-from torch_geometric.nn.aggr import LSTMAggregation
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.metrics import f1_score
+from torch_geometric.nn.aggr import MaxAggregation
+from sklearn.metrics import mean_squared_error
 
 PATH_GRAPHS = os.path.join(PATH_DATA_INTERIM, 'is_male')
 
@@ -60,91 +59,58 @@ test = DataLoader(
     shuffle=False)
 
 model = GraphSAGE(**params_model).to(device)
-agg_fun = LSTMAggregation(**params_agg_lstm).to(device)
+agg_fun = MaxAggregation()
 
 optimizer = torch.optim.Adam(
     model.parameters(),
     lr=params['lr'],
     weight_decay=params['weight_decay'])
-loss_fun = torch.nn.CrossEntropyLoss()
+loss_fun = torch.nn.MSELoss()
 
 
 if __name__ == '__main__':
 
-    mlflow.set_experiment('GNN')
+    mlflow.set_experiment('GNN_is_male')
     mlflow.start_run()
 
-    list_out_03_test = []
-    list_out_04_test = []
-    list_out_05_test = []
-    list_out_06_test = []
-    list_out_07_test = []
+    list_out_test = []
     list_y_test = []
     for epoch in range(1, params['n_epochs']+1):
 
-        list_out_03_train = []
-        list_out_04_train = []
-        list_out_05_train = []
-        list_out_06_train = []
-        list_out_07_train = []
+        list_out_train = []
         list_y_train = []
 
-        list_out_03_val = []
-        list_out_04_val = []
-        list_out_05_val = []
-        list_out_06_val = []
-        list_out_07_val = []
+        list_out_val = []
         list_y_val = []
 
         print(f"[Epoch: {epoch}]")
 
         model.train()
-        agg_fun.train()
         for batch in tqdm(
                 train, total=len(train), colour='green'):
 
             out = model(batch.x, batch.edge_index)
             out = agg_fun(out, batch.batch)
-            out = torch.sigmoid(out)
-            y = batch.y.type(torch.cuda.ByteTensor)
+            out = torch.sigmoid(out).reshape(-1)
+            y = batch.y.type(torch.cuda.FloatTensor)
             loss = loss_fun(out, y)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            out_03 = [1 if x.item() >= 0.3 else 0 for x in out]
-            out_04 = [1 if x.item() >= 0.4 else 0 for x in out]
-            out_05 = [1 if x.item() >= 0.5 else 0 for x in out]
-            out_06 = [1 if x.item() >= 0.6 else 0 for x in out]
-            out_07 = [1 if x.item() >= 0.7 else 0 for x in out]
+            out = [x.item() for x in out]
             y = list(y.cpu().numpy())
 
-            list_out_03_train.extend(out_03)
-            list_out_04_train.extend(out_04)
-            list_out_05_train.extend(out_05)
-            list_out_06_train.extend(out_06)
-            list_out_07_train.extend(out_07)
+            list_out_train.extend(out)
             list_y_train.extend(y)
 
-        f1_train_03 = f1_score(
-            y_true=list_y_train,
-            y_pred=list_out_03_train)
-        f1_train_04 = f1_score(
-            y_true=list_y_train,
-            y_pred=list_out_04_train)
-        f1_train_05 = f1_score(
-            y_true=list_y_train,
-            y_pred=list_out_05_train)
-        f1_train_06 = f1_score(
-            y_true=list_y_train,
-            y_pred=list_out_06_train)
-        f1_train_07 = f1_score(
-            y_true=list_y_train,
-            y_pred=list_out_07_train)
+        rmse_train = mean_squared_error(
+            y_true=list_y_train, y_pred=list_out_train, squared=False)
+        mlflow.log_metric(
+            key='rmse_train', value=rmse_train, step=epoch)
 
         model.eval()
-        agg_fun.eval()
         with torch.no_grad():
             for batch in tqdm(
                     val, total=len(val), colour='green'):
@@ -152,187 +118,43 @@ if __name__ == '__main__':
                 out = model(batch.x, batch.edge_index)
                 out = agg_fun(out, batch.batch)
                 out = torch.sigmoid(out)
-                y = batch.y.type(torch.cuda.ByteTensor)
+                y = batch.y
 
-                out_03 = [1 if x.item() >= 0.3 else 0 for x in out]
-                out_04 = [1 if x.item() >= 0.4 else 0 for x in out]
-                out_05 = [1 if x.item() >= 0.5 else 0 for x in out]
-                out_06 = [1 if x.item() >= 0.6 else 0 for x in out]
-                out_07 = [1 if x.item() >= 0.7 else 0 for x in out]
+                out = [x.item() for x in out]
                 y = list(y.cpu().numpy())
 
-                list_out_03_val.extend(out_03)
-                list_out_04_val.extend(out_04)
-                list_out_05_val.extend(out_05)
-                list_out_06_val.extend(out_06)
-                list_out_07_val.extend(out_07)
+                list_out_val.extend(out)
                 list_y_val.extend(y)
 
-        f1_val_03 = f1_score(
-            y_true=list_y_val,
-            y_pred=list_out_03_val)
-        f1_val_04 = f1_score(
-            y_true=list_y_val,
-            y_pred=list_out_04_val)
-        f1_val_05 = f1_score(
-            y_true=list_y_val,
-            y_pred=list_out_05_val)
-        f1_val_06 = f1_score(
-            y_true=list_y_val,
-            y_pred=list_out_06_val)
-        f1_val_07 = f1_score(
-            y_true=list_y_val,
-            y_pred=list_out_07_val)
-
+        rmse_val = mean_squared_error(
+            y_true=list_y_val, y_pred=list_out_val, squared=False)
         mlflow.log_metric(
-            key='f1_train_03', value=f1_train_03, step=epoch)
-        mlflow.log_metric(
-            key='f1_train_04', value=f1_train_04, step=epoch)
-        mlflow.log_metric(
-            key='f1_train_05', value=f1_train_05, step=epoch)
-        mlflow.log_metric(
-            key='f1_train_06', value=f1_train_06, step=epoch)
-        mlflow.log_metric(
-            key='f1_train_07', value=f1_train_07, step=epoch)
-        mlflow.log_metric(
-            key='f1_val_03', value=f1_val_03, step=epoch)
-        mlflow.log_metric(
-            key='f1_val_04', value=f1_val_04, step=epoch)
-        mlflow.log_metric(
-            key='f1_val_05', value=f1_val_05, step=epoch)
-        mlflow.log_metric(
-            key='f1_val_06', value=f1_val_06, step=epoch)
-        mlflow.log_metric(
-            key='f1_val_07', value=f1_val_07, step=epoch)
+            key='rmse_val', value=rmse_val, step=epoch)
 
     model.eval()
-    agg_fun.eval()
     with torch.no_grad():
         for batch in tqdm(
                 test, total=len(test), colour='green'):
 
             out = model(batch.x, batch.edge_index)
-            out = model(batch.x, batch.edge_index)
             out = agg_fun(out, batch.batch)
             out = torch.sigmoid(out)
-            y = batch.y.type(torch.cuda.ByteTensor)
+            y = batch.y
 
-            out_03 = [1 if x.item() >= 0.3 else 0 for x in out]
-            out_04 = [1 if x.item() >= 0.4 else 0 for x in out]
-            out_05 = [1 if x.item() >= 0.5 else 0 for x in out]
-            out_06 = [1 if x.item() >= 0.6 else 0 for x in out]
-            out_07 = [1 if x.item() >= 0.7 else 0 for x in out]
+            out = [x.item() for x in out]
             y = list(y.cpu().numpy())
 
-            list_out_03_test.extend(out_03)
-            list_out_04_test.extend(out_04)
-            list_out_05_test.extend(out_05)
-            list_out_06_test.extend(out_06)
-            list_out_07_test.extend(out_07)
+            list_out_test.extend(out)
             list_y_test.extend(y)
 
-    f1_test_03 = f1_score(
-        y_true=list_y_test,
-        y_pred=list_out_03_test)
-    f1_test_04 = f1_score(
-        y_true=list_y_test,
-        y_pred=list_out_04_test)
-    f1_test_05 = f1_score(
-        y_true=list_y_test,
-        y_pred=list_out_05_test)
-    f1_test_06 = f1_score(
-        y_true=list_y_test,
-        y_pred=list_out_06_test)
-    f1_test_07 = f1_score(
-        y_true=list_y_test,
-        y_pred=list_out_07_test)
-
+    rmse_test = mean_squared_error(
+        y_true=list_y_test, y_pred=list_out_test, squared=False)
     mlflow.log_metric(
-        key='f1_test_03', value=f1_test_03, step=epoch)
-    mlflow.log_metric(
-        key='f1_test_04', value=f1_test_04, step=epoch)
-    mlflow.log_metric(
-        key='f1_test_05', value=f1_test_05, step=epoch)
-    mlflow.log_metric(
-        key='f1_test_06', value=f1_test_06, step=epoch)
-    mlflow.log_metric(
-        key='f1_test_07', value=f1_test_07, step=epoch)
-
-    conf_matrix_03 = confusion_matrix(list_y_test, list_out_03_test)
-    conf_matrix_04 = confusion_matrix(list_y_test, list_out_04_test)
-    conf_matrix_05 = confusion_matrix(list_y_test, list_out_05_test)
-    conf_matrix_06 = confusion_matrix(list_y_test, list_out_06_test)
-    conf_matrix_07 = confusion_matrix(list_y_test, list_out_07_test)
-
-    conf_matrix_03_norm = confusion_matrix(
-        list_y_test, list_out_03_test, normalize='true')
-    conf_matrix_04_norm = confusion_matrix(
-        list_y_test, list_out_04_test, normalize='true')
-    conf_matrix_05_norm = confusion_matrix(
-        list_y_test, list_out_05_test, normalize='true')
-    conf_matrix_06_norm = confusion_matrix(
-        list_y_test, list_out_06_test, normalize='true')
-    conf_matrix_07_norm = confusion_matrix(
-        list_y_test, list_out_07_test, normalize='true')
-
-    plot_03 = ConfusionMatrixDisplay(conf_matrix_03).plot().figure_
-    plot_04 = ConfusionMatrixDisplay(conf_matrix_04).plot().figure_
-    plot_05 = ConfusionMatrixDisplay(conf_matrix_05).plot().figure_
-    plot_06 = ConfusionMatrixDisplay(conf_matrix_06).plot().figure_
-    plot_07 = ConfusionMatrixDisplay(conf_matrix_07).plot().figure_
-    plot_norm_03 = ConfusionMatrixDisplay(conf_matrix_03_norm).plot().figure_
-    plot_norm_04 = ConfusionMatrixDisplay(conf_matrix_04_norm).plot().figure_
-    plot_norm_05 = ConfusionMatrixDisplay(conf_matrix_05_norm).plot().figure_
-    plot_norm_06 = ConfusionMatrixDisplay(conf_matrix_06_norm).plot().figure_
-    plot_norm_07 = ConfusionMatrixDisplay(conf_matrix_07_norm).plot().figure_
-
-    plot_03.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_03.png'))
-    plot_04.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_04.png'))
-    plot_05.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_05.png'))
-    plot_06.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_06.png'))
-    plot_07.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_07.png'))
-    plot_norm_03.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_03.png'))
-    plot_norm_04.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_04.png'))
-    plot_norm_05.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_05.png'))
-    plot_norm_06.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_06.png'))
-    plot_norm_07.savefig(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_07.png'))
-
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_03.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_04.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_05.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_06.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_07.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_03.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_04.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_05.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_06.png'))
-    mlflow.log_artifact(
-        os.path.join(PATH_DATA_INTERIM, 'conf_matrix_norm_07.png'))
+        key='rmse_test', value=rmse_test, step=epoch)
 
     save_f(filename=os.path.join(PATH_DATA_INTERIM, 'model.pkl'), obj=model)
     mlflow.log_artifact(os.path.join(PATH_DATA_INTERIM, 'model.pkl'))
 
     mlflow.log_params(params=params)
-    mlflow.log_params(params=params_model)
-    mlflow.log_params(params=params_agg_lstm)
 
     mlflow.end_run()
