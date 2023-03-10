2008b070f2e20bb96ed57835da9dc17bacc642d2
Rustem <mr.kamilyanov@mail.ru>
2023-03-10 17:55:47 +0300

    interim

--- src/lib/params_is_male.py
+++ src/lib/params_is_male.py
@@ -1,8 +1,8 @@
 params = {
     'device': 'cuda',
     'batch_size': 16,
-    'lr': 5e-5,
-    'weight_decay': 5e-5,
+    'lr': 5e-4,
+    'weight_decay': 5e-4,
     'n_epochs': 20
 }
 params_model = {
