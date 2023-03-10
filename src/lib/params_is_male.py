4d3ce479c64c621c623feceb20c1bf9784daea01
Rustem <mr.kamilyanov@mail.ru>
2023-03-10 17:42:33 +0300

    .

--- src/lib/params_is_male.py
+++ src/lib/params_is_male.py
@@ -3,14 +3,14 @@ params = {
     'batch_size': 16,
     'lr': 5e-5,
     'weight_decay': 5e-5,
-    'n_epochs': 10
+    'n_epochs': 20
 }
 params_model = {
     'in_channels': 18,
     'hidden_channels': 100,
     'num_layers': 3,
     'out_channels': 1,
-    'dropout': 0.2,
+    'dropout': 0.3,
     'act': 'relu',
     'aggr': 'max',
     'jk': 'cat'
