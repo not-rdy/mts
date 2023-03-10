14ec6b305a70ff20c4e69299fdb3a42708e035c6
Rustem <mr.kamilyanov@mail.ru>
2023-03-10 17:14:40 +0300

    interim

--- src/lib/params_is_male.py
+++ src/lib/params_is_male.py
@@ -1,14 +1,14 @@
 params = {
     'device': 'cuda',
-    'batch_size': 16,
-    'lr': 5e-4,
+    'batch_size': 32,
+    'lr': 5e-5,
     'weight_decay': 5e-6,
     'n_epochs': 20
 }
 params_model = {
     'in_channels': 18,
     'hidden_channels': 100,
-    'num_layers': 4,
+    'num_layers': 2,
     'out_channels': 1,
     'dropout': 0.2,
     'act': 'relu',
