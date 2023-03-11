b11b353099a5e83baa405daac8bf8c5859106e36
Rustem <mr.kamilyanov@mail.ru>
2023-03-11 23:11:01 +0300

    .

--- src/lib/params_age.py
+++ src/lib/params_age.py
@@ -3,13 +3,13 @@ params = {
     'batch_size': 64,
     'lr': 5e-5,
     'weight_decay': 5e-5,
-    'n_epochs': 1000,
+    'n_epochs': 20,
     'weights_loss': [0.151, 0.0152, 0.079, 0.134, 0.27, 0.3508]
 }
 params_model = {
     'in_channels': 18,
     'hidden_channels': 100,
-    'num_layers': 10,
+    'num_layers': 15,
     'out_channels': 100,
     'dropout': 0.3,
     'act': 'relu',
