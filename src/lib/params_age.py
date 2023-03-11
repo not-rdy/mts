37d5095091ece960651933f2a6e6f3970cacaadf
Rustem <mr.kamilyanov@mail.ru>
2023-03-11 23:29:47 +0300

    .

--- src/lib/params_age.py
+++ src/lib/params_age.py
@@ -9,7 +9,7 @@ params = {
 params_model = {
     'in_channels': 18,
     'hidden_channels': 100,
-    'num_layers': 15,
+    'num_layers': 10,
     'out_channels': 100,
     'dropout': 0.3,
     'act': 'relu',
