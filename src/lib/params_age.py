82c3c339bedca8b48e01e3c95e75a21ec500f734
Rustem <mr.kamilyanov@mail.ru>
2023-03-11 19:29:07 +0300

    interim

--- src/lib/params_age.py
+++ src/lib/params_age.py
@@ -1,7 +1,7 @@
 params = {
     'device': 'cuda',
     'batch_size': 64,
-    'lr': 5e-4,
+    'lr': 5e-5,
     'weight_decay': 5e-5,
     'n_epochs': 100,
     'weights_loss': [0.151, 0.0152, 0.079, 0.134, 0.27, 0.3508]
@@ -9,7 +9,7 @@ params = {
 params_model = {
     'in_channels': 18,
     'hidden_channels': 100,
-    'num_layers': 6,
+    'num_layers': 10,
     'out_channels': 6,
     'dropout': 0.3,
     'act': 'relu',
