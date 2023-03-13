3b1d007c7853e1a9f332fab37dc0d5c6b7f0a66b
Rustem <mr.kamilyanov@mail.ru>
2023-03-13 17:41:01 +0300

    interim

--- src/lib/params_age.py
+++ src/lib/params_age.py
@@ -1,9 +1,9 @@
 params = {
     'device': 'cuda',
-    'batch_size': 32,
+    'batch_size': 16,
     'lr': 5e-4,
     'weight_decay': 5e-5,
-    'n_epochs': 100,
+    'n_epochs': 20,
     'weights_loss': [0.151, 0.0152, 0.079, 0.134, 0.27, 0.3508]
 }
 params_model = {
