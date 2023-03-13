fd82673304e31007c863051fae472b5a5d74ddbb
Rustem <mr.kamilyanov@mail.ru>
2023-03-13 19:46:04 +0300

    interim

--- src/lib/params_age.py
+++ src/lib/params_age.py
@@ -1,21 +1,19 @@
 from torch_geometric.nn.norm import BatchNorm
 
-norm = BatchNorm(
-    in_channels=100,
-    momentum=0.01)
+norm = BatchNorm(in_channels=100)
 
 params = {
     'device': 'cuda',
     'batch_size': 32,
     'lr': 5e-4,
     'weight_decay': 5e-5,
-    'n_epochs': 100,
+    'n_epochs': 20,
     'weights_loss': [0.151, 0.0152, 0.079, 0.134, 0.27, 0.3508]
 }
 params_model = {
     'in_channels': 18,
     'hidden_channels': 100,
-    'num_layers': 3,
+    'num_layers': 4,
     'out_channels': 100,
     'dropout': 0.3,
     'act': 'relu',
